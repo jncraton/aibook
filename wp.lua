d58dbff459b1e3dc7775445d5012398447b0f50c
Jon Craton <jncraton@gmail.com>
2026-06-15 14:31:05 -0400

    Add wp.lua

---
+++ wp.lua
@@ -0,0 +1,14 @@
+function Strong(elem)
+  -- 1. Ensure the bold element actually has text content
+  if elem.content and #elem.content > 0 then
+    -- 2. Convert the bold content into a plain string
+    local link_text = pandoc.utils.stringify(elem.content)
+    
+    -- 3. Replace spaces with underscores for the Wikipedia URL
+    local search_term = link_text:gsub(" ", "_")
+    local url = "https://en.wikipedia.org/wiki/" .. search_term
+    
+    -- 4. Return a new Link element containing the original bold text
+    return pandoc.Link(elem.content, url)
+  end
+end
